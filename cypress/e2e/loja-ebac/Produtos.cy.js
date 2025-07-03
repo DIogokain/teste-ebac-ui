44cf7e1de384c377be27fc68fdbce2c9b86f68a5
Fabiano <fabianodiogo8545@gmail.com>
2025-07-03 20:14:26 -0300

    faker prudutos

---
+++ cypress/e2e/loja-ebac/Produtos.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidades: Produtos', () => {
+    beforeEach(() => {
+         cy.visit('http://lojaebac.ebaconline.art.br/produtos/');
+    
+    });
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block').last().click();
+
+        cy
+        
+    });
+
+});
